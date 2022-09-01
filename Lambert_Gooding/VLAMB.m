07b0734dd20636cbeba9b1f9cbfcac08b4585fda
Jeff Ko <2929up@gmail.com>
2022-09-02 00:54:54 +0900

    Gooding Algorithm 기반 Lambert Rendezvous Del V 계산 코드 _MATLAB 구현 완료

--- Lambert_Gooding/VLAMB.m
+++ Lambert_Gooding/VLAMB.m
@@ -1,30 +1,26 @@
-function [N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = VLAMB(GM, R1, R2, TH, TDELT)
+function [N, Vr11, Vt11, Vr12, Vt12, Vr21, Vt21, Vr22, Vt22] = VLAMB(Mu, r1, r2, theta, delT)
 
-VR11 = NaN; VT11 = NaN; VR12 = NaN; VT12 = NaN;
-VR21 = NaN; VT21 = NaN; VR22 = NaN; VT22 = NaN;
+Vr11 = NaN; Vt11 = NaN; Vr12 = NaN; Vt12 = NaN;
+Vr21 = NaN; Vt21 = NaN; Vr22 = NaN; Vt22 = NaN;
 % return NaN for N<1 case
 
-TWOPI=2*pi;
-M = floor(TH/TWOPI);
-THR2 = TH/2 - M*pi;
-DR = R1-R2;
-R1R2 = R1*R2;
-R1R2TH = 4*R1R2*sin(THR2)^2;
-CSQ = DR^2 + R1R2TH;
-C = sqrt(CSQ);
-S = (R1 + R2 + C) /2;
-GMS = sqrt(GM*S/2);
-QSQFM1 = C/S;
-Q = sqrt(R1R2) * cos(THR2) /S;
-if(C~=0)
-    RHO = DR/C;
-    SIG = R1R2TH/CSQ;
+m = floor(theta/(2*pi)); %number of revolution
+theta2 = theta/2 - m*pi; % normalized value of theta
+cSqr = (r1-r2)^2 + 4*r1*r2*sin(theta2)^2; % c^2
+c = sqrt(cSqr); % chord length
+s = (r1 + r2 + c) /2; % 둘레
+GMS = sqrt(Mu*s/2);
+qSqrFM1 = c/s; %1-q^2
+q = sqrt(r1*r2) * cos(theta2) /s;
+if(c~=0)
+    RHO = (r1-r2)/c;
+    SIG = 4*r1*r2*sin(theta2)^2/cSqr;
 else
     RHO=0;
     SIG=1;
 end
-T = 4*GMS*TDELT/S^2;
-[N,X1,X2] = XLAMB(M,Q,QSQFM1,T);
+T = 4*GMS*delT/s^2;
+[N,X1,X2] = XLAMB(m,q,qSqrFM1,T);
 % PROCEED FOR SINGLE SOLUTION, OR A PAIR
 for I=1:N
     if (I==1)
@@ -32,117 +28,117 @@ for I=1:N
     else
         X=X2;
     end
-    [~, QZMINX, QZPLX, ZPLQX]=TLAMB(M,Q,QSQFM1,X,-1);
-    VT2 = GMS*ZPLQX*sqrt(SIG);
-    VR1 = GMS*(QZMINX - QZPLX*RHO)/R1;
-    VT1 = VT2/R1;
-    VR2 = -GMS*(QZMINX + QZPLX*RHO) /R2;
-    VT2 = VT2/R2;
+    [~, QZMINX, QZPLX, ZPLQX]=TLAMB(m,q,qSqrFM1,X,-1);
+    Vt2 = GMS*ZPLQX*sqrt(SIG);
+    Vr1 = GMS*(QZMINX - QZPLX*RHO)/r1;
+    Vt1 = Vt2/r1;
+    Vr2 = -GMS*(QZMINX + QZPLX*RHO) /r2;
+    Vt2 = Vt2/r2;
     if (I==1)
-        VR11 = VR1;
-        VT11 = VT1;
-        VR12 = VR2;
-        VT12 = VT2;
+        Vr11 = Vr1;
+        Vt11 = Vt1;
+        Vr12 = Vr2;
+        Vt12 = Vt2;
     else
-        VR21 = VR1;
-        VT21 = VT1;
-        VR22 = VR2;
-        VT22 = VT2;
+        Vr21 = Vr1;
+        Vt21 = Vt1;
+        Vr22 = Vr2;
+        Vt22 = Vt2;
     end
 end
 
 end
 
-function [N,X,XPL] = XLAMB(M,Q,QSQFM1,TIN)
-X=NaN; % N=0 인 경우 X 값으로 NaN 반환
-XPL=NaN; % N=1 인 경우 XPL 값으로 NaN 반환
-TOL = 3*10^-7;
+function [N,x0,x00] = XLAMB(m,q,qSqrFM1,TIn)
+x0=NaN; % N=0 인 경우 X0 값으로 NaN 반환
+x00=NaN; % N=1 인 경우 x00 값으로 NaN 반환
+toler = 3*10^-7;
 C0 = 1.7;
 C1 = 0.5;
 C2 = 0.03;
 C3 = 0.15;
 C41 = 1;
 C42 = 0.24;
-THR2 = atan2(QSQFM1, 2*Q)/pi;
-if (M==0)
+theta2 = atan2(qSqrFM1, 2*q)/pi;
+if (m==0)
     %SINGLE REVOLUTION STARTER FROM T
     N=1;
-    [T0,~,~,~]=TLAMB(M,Q,QSQFM1,0,0);
-    TDIFF = TIN - T0;
-    if (TDIFF <= 0)
-        X = T0*TDIFF / (-4+TIN);
+    [T0,~,~,~]=TLAMB(m,q,qSqrFM1,0,0);
+    TDiff = TIn - T0;
+    if (TDiff <= 0)
+        x0 = T0*TDiff / (-4+TIn);
     else
-        X = -TDIFF/(TDIFF+4);
-        W = X+C0*sqrt(2*(1-THR2));
+        x0 = -TDiff/(TDiff+4);
+        W = x0+C0*sqrt(2*(1-theta2));
         if (W<0)
-            X = X - sqrt (D8RT(-W))*(X+sqrt(TDIFF/(TDIFF + 1.5*T0)));
+            x0 = x0 - sqrt ((-W)^(1/8))*(x0+sqrt(TDiff/(TDiff + 1.5*T0)));
         end
-        W = 4 / (4+TDIFF);
-        X = X * (1+X*(C1*W - C2*X*sqrt(W)));
+        W = 4 / (4+TDiff);
+        x0 = x0 * (1+x0*(C1*W - C2*x0*sqrt(W)));
     end
 else
     %multirevs
-    XM = 1/(1.5*(M+0.5)*pi);
-    if (THR2 < 0.5)
-        XM = D8RT(2*THR2)*XM;
+    xm = 1/(1.5*(m+0.5)*pi);
+    if (theta2 < 0.5)
+        xm = (2*theta2)^(1/8)*xm;
     end
-    if (THR2 > 0.5 )
-        XM = (2 * D8RT(2-2*THR2))*XM;
+    if (theta2 > 0.5 )
+        xm = (2 * (2-2*theta2)^(1/8))*xm;
     end
-    for I=1:12
-        [TMIN,DT,D2T,D3T] = TLAMB(M,Q,QSQFM1,XM,3);
-        if (D2T == 0)
+    for i=1:12
+        [TMin,dT,d2T,d3T] = TLAMB(m,q,qSqrFM1,xm,3);
+        if (d2T == 0)
             break;
         end
-        XMOLD = XM;
-        XM = XM - DT*D2T/(D2T*D2T-DT*D3T/2);
-        XTEST = abs(XMOLD/XM - 1);
-        if (XTEST<=TOL)
+        xmOld = xm;
+        xm = xm - dT*d2T/(d2T*d2T-dT*d3T/2);
+        xTest = abs(xmOld/xm - 1);
+        if (xTest<=toler)
             break;
         end
-        if (I==12)
+        if (i==12)
             N=-1;
             return;
         end
     end
-    TDIFFM = TIN - TMIN;
-    if (TDIFFM<0)
+    TDiffM = TIn - TMin;
+    if (TDiffM<0)
         N=0;
         return;
-    elseif(TDIFFM==0)
-            X = XM;
+    elseif(TDiffM==0)
+            x0 = xm;
             N = 1;
             return;
     else
         N = 3;
-        if (D2T==0)
-            D2T = 6*M*pi;
+        if (d2T==0)
+            d2T = 6*m*pi;
         end
-        X = sqrt(TDIFFM/(D2T/2 + TDIFFM/(1-XM)^2));
-        W = XM + X;
-        W = W*4/(4+TDIFFM) + (1-W)^2;
-        X = X*(1-(1+M+C41*(THR2-0.5))/(1+C3*M)*X*(C1*W + C2*X*sqrt(W))) + XM;
-        D2T2 = D2T/2;
-        if (X>=1)
+        x0 = sqrt(TDiffM/(d2T/2 + TDiffM/(1-xm)^2));
+        W = xm + x0;
+        W = W*4/(4+TDiffM) + (1-W)^2;
+        x0 = x0*(1-(1+m+C41*(theta2-0.5))/(1+C3*m)*x0*(C1*W + C2*x0*sqrt(W))) + xm;
+        d2T2 = d2T/2;
+        if (x0>=1)
             N=1;
             %go to 3
-            [T0,~,~,~] = TLAMB(M,Q,QSQFM1,0,0);
-            TDIFF0 = T0 - TMIN;
-            TDIFF = TIN - T0;
-            if (TDIFF<=0)
-                X = XM - sqrt(TDIFFM/(D2T2-TDIFFM*(D2T2/TDIFF0-1/XM^2)));
+            [T0,dT,d2T,d3T] = TLAMB(m,q,qSqrFM1,0,0);
+            TDiff0 = T0 - TMin;
+            TDiff = TIn - T0;
+            if (TDiff<=0)
+                x0 = xm - sqrt(TDiffM/(d2T2-TDiffM*(d2T2/TDiff0-1/xm^2)));
             else
-                X= -TDIFF/(TDIFF+4);
-                W = X + C0* sqrt(2*(1*THR2));
+                x0= -TDiff/(TDiff+4);
+                W = x0 + C0* sqrt(2*(1*theta2));
                 if (W<0)
-                    X = X - sqrt(D8RT(-W))*(X+sqrt(TDIFF/(TDIFF+1.5*T0)));
+                    x0 = x0 - sqrt((-W)^(1/8))*(x0+sqrt(TDiff/(TDiff+1.5*T0)));
                 end
-                W = 4/(4+TDIFF);
-                X = X*(1+(1+M+C42*(THR2-0.5))/(1+C3*M)*X*(C1*W-C2*X*sqrt(W)));
-                if (X<=-1)
+                W = 4/(4+TDiff);
+                x0 = x0*(1+(1+m+C42*(theta2-0.5))/(1+C3*m)*x0*(C1*W-C2*x0*sqrt(W)));
+                if (x0<=-1)
                     N=N-1;
                     if (N==1)
-                        X = XPL;
+                        x0 = x00;
                     end
                 end
             end
@@ -151,261 +147,191 @@ else
 end
 %starting condition 완료
 while(1)
-    for I=1:3
-        [T,DT,D2T,D3T] = TLAMB(M,Q,QSQFM1,X,2);
-        T = TIN-T;
-        if (DT~=0)
-            X = X + T*DT/ (DT*DT+T*D2T/2);
+    for i=1:3
+        [T,dT,d2T,~] = TLAMB(m,q,qSqrFM1,x0,2);
+        T = TIn-T;
+        if (dT~=0)
+            x0 = x0 + T*dT/ (dT*dT+T*d2T/2);
         end
     end
     if (N~=3)
         return; % Exit if only one solution, normally when M=0
     end
     N=2;
-    XPL=X;
-    %논문의 3 구현..
-    [T0,~,~,~] = TLAMB(M,Q,QSQFM1,0,0);
-    TDIFF0 = T0 - TMIN;
-    TDIFF = TIN - T0;
-    if (TDIFF<=0)
-        X = XM - sqrt(TDIFFM/(D2T2-TDIFFM*(D2T2/TDIFF0-1/XM^2)));
+    x00=x0;
+    [T0,~,~,~] = TLAMB(m,q,qSqrFM1,0,0);
+    TDiff0 = T0 - TMin;
+    TDiff = TIn - T0;
+    if (TDiff<=0)
+        x0 = xm - sqrt(TDiffM/(d2T2-TDiffM*(d2T2/TDiff0-1/xm^2)));
     else
-        X= -TDIFF/(TDIFF+4);
-        W = X + C0* sqrt(2*(1*THR2));
+        x0= -TDiff/(TDiff+4);
+        W = x0 + C0* sqrt(2*(1*theta2));
         if (W<0)
-            X = X - sqrt(D8RT(-W))*(X+sqrt(TDIFF/(TDIFF+1.5*T0)));
+            x0 = x0 - sqrt((-W)^(1/8))*(x0+sqrt(TDiff/(TDiff+1.5*T0)));
         end
-        W = 4/(4+TDIFF);
-        X = X*(1+(1+M+C42*(THR2-0.5))/(1+C3*M)*X*(C1*W-C2*X*sqrt(W)));
-        if (X<=-1)
+        W = 4/(4+TDiff);
+        x0 = x0*(1+(1+m+C42*(theta2-0.5))/(1+C3*m)*x0*(C1*W-C2*x0*sqrt(W)));
+        if (x0<=-1)
             N=N-1;
             if (N==1)
-                X = XPL;
+                x0 = x00;
             end
         end
     end
 end
-
-for i=1:3
-    [T,DT,D2T,~] = TLAMB(M,Q,QSQFM1,X,2);
-    T = TIN - T;
-    if (DT~=0)
-        X = X + T*DT/(DT*DT + T*D2T/2);
-    end
-end
-if(N~=3)
-    return;
-end
-N=2;
-XPL = X;
-[T0,~,~,~] = TLAMB(M,Q,QSQFM1,0,0);
-TDIFF0 = T0 - TMIN;
-TDIFF = TIN - T0;
-if (TDIFF<=0)
-    X = XM - sqrt(TDIFFM/(D2T2-TDIFFM*(D2T2/TDIFF0-1/XM^2)));
-else
-    X= -TDIFF/(TDIFF+4);
-    W = X + C0* sqrt(2*(1*THR2));
-    if (W<0)
-        X = X - sqrt(D8RT(-W))*(X+sqrt(TDIFF/(TDIFF+1.5*T0)));
-    end
-    W = 4/(4+TDIFF);
-    X = X*(1+(1+M+C42*(THR2-0.5))/(1+C3*M)*X*(C1*W-C2*X*sqrt(W)));
-    if (X<=-1)
-        N=N-1;
-        if (N==1)
-            X = XPL;
-        end
-    end
-end
-
 end
 
 
-function d8rt = D8RT(x) 
-d8rt= sqrt(sqrt(sqrt(x)));
-end
-
-function [T,DT,D2T,D3T] = TLAMB(M,Q,QSQFM1,X,N)
+function [T,dT,d2T,d3T] = TLAMB(m,q,qSqrFM1,x,nDot)
 T=NaN; % N=-1 인 경우, T= NaN 으로 출력
-SW = 0.4;
-LM1 = (N==-1);
-L1 = (N>=1);
-L2 = (N>=2);
-L3 = (N==3);
-QSQ = Q*Q;
-XSQ = X*X;
-U = (1-X)*(1+X); % -E
+LM1 = (nDot==-1);
+L1 = (nDot>=1);
+L2 = (nDot>=2);
+L3 = (nDot==3);
+qSqr= q*q;
+xSqr = x*x;
+U = (1-x)*(1+x); % -E
 if (~LM1)
     %needed if series, and otherwise usful when z =0
-    DT = 0;
-    D2T = 0;
-    D3T = 0;
+    dT = 0;
+    d2T = 0;
+    d3T = 0;
 end
-if (LM1 || (M>0) || (X<0) || (abs(U)>SW))
+if (LM1 || (m>0) || (x<0) || (abs(U)>0.4))
     %direct computation (not series)
-    Y = sqrt(abs(U));
-    Z = sqrt(QSQFM1 + QSQ*XSQ);
-    QX = Q*X;
-    if (QX<=0)
-        A = Z - QX;
-        B = Q*Z - X;
+    y = sqrt(abs(U));
+    z = sqrt(qSqrFM1 + qSqr*xSqr);
+    qx = q*x;
+    if (qx<=0)
+        A = z - qx;
+        B = q*z - x;
     end
-    if (QX<0 && LM1)
-        AA = QSQFM1 / A;
-        BB = QSQFM1 * (QSQ*U - XSQ)/B;
+    if (qx<0 && LM1)
+        AA = qSqrFM1 / A;
+        BB = qSqrFM1 * (qSqr*U - xSqr)/B;
     end
-    if (((QX==0) && LM1) || (QX>0))
-        AA = Z + QX;
-        BB = Q*Z + X;
+    if (((qx==0) && LM1) || (qx>0))
+        AA = z + qx;
+        BB = q*z + x;
     end
-    if (QX>0)
-        A = QSQFM1/AA;
-        B = QSQFM1*(QSQ*U - XSQ) /BB;
+    if (qx>0)
+        A = qSqrFM1/AA;
+        B = qSqrFM1*(qSqr*U - xSqr) /BB;
     end
     if (~LM1)
-        if (QX*U >= 0)
-            G = X*Z + Q*U;
+        if (qx*U >= 0)
+            G = x*z + q*U;
         else
-            G = (XSQ - QSQ*U) / (X*Z - Q*U);
+            G = (xSqr - qSqr*U) / (x*z - q*U);
         end
-        F = A*Y;
-        if (X<=1)
-            T = M*pi + atan2(F,G);
+        F = A*y;
+        if (x<=1)
+            T = m*pi + atan2(F,G);
         else
-            if(F>SW)
+            if(F>0.4)
                 T = log(F+G);
             else
                 FG1 = F/(G+1);
                 TERM = 2*FG1;
-                FG1SQ = FG1*FG1;
+                FG1Sqr = FG1*FG1;
                 T = TERM;
                 TWOI1 = 1;
                 TWOI1 = TWOI1 + 2;
-                TERM = TERM * FG1SQ;
-                TOLD = T;
+                TERM = TERM * FG1Sqr;
+                TOld = T;
                 T = T + TERM/TWOI1;
-                while(T~=TOLD)
+                while(T~=TOld)
                     TWOI1 = TWOI1 + 2;
-                    TERM = TERM * FG1SQ;
-                    TOLD = T;
+                    TERM = TERM * FG1Sqr;
+                    TOld = T;
                     T = T + TERM/TWOI1;
                 end
             end
         end
-        T = 2 * (T/Y + B)/U;
-        if (L1 && (Z~=0))
-            QZ = Q/Z;
-            QZ2 = QZ*QZ;
-            QZ = QZ*QZ2;
-            DT = (3*X*T - 4*(A + QX*QSQFM1) / Z) /U;
+        T = 2 * (T/y + B)/U;
+        if (L1 && (z~=0))
+            dT = (3*x*T - 4*(A + qx*qSqrFM1) / z) /U;
             if (L2)
-                D2T= ( 3*T + 5*X*DT + 4*QZ*QSQFM1 ) /U;
+                d2T= ( 3*T + 5*x*dT + 4*(q/z)^3*qSqrFM1 ) /U;
             end
             if (L3)
-                D3T = (8*DT+7*X*D2T-12*QZ*QZ2*X*QSQFM1)/U;
+                d3T = (8*dT+7*x*d2T-12*(q/z)^5*x*qSqrFM1)/U;
             end
         end
     else
-        DT=B;
-        D2T = BB;
-        D3T = AA;
+        dT=B;
+        d2T = BB;
+        d3T = AA;
     end
 else
     %compute by series
-    U0I=1;
+    u0=1;
     if (L1)
-        U1I = 1;
+        u1 = 1;
     end
     if (L2)
-        U2I = 1;
+        u2 = 1;
     end
     if (L3)
-        U3I = 1;
+        u3 = 1;
     end
     TERM = 4;
-    TQ = Q*QSQFM1;
+    TQ = q*qSqrFM1;
     I = 0;
-    if (Q<5/10)
-        TQSUM = 1 - Q*QSQ;
+    if (q<5/10)
+        TQSUM = 1 - q*qSqr;
     end
-    if (Q>=5/10)
-        TQSUM = (1/(1+Q) + Q)*QSQFM1;
+    if (q>=5/10)
+        TQSUM = (1/(1+q) + q)*qSqrFM1;
     end
     TTMOLD = TERM/3;
     T = TTMOLD*TQSUM;
     % Start of Loop
-    I = I + 1;
-    P = I;
-    U0I = U0I*U;
-    if (L1 && (I>1))
-        U1I = U1I*U;
-    end
-    if (L2 && (I>2))
-        U2I = U2I*U;
-    end
-    if (L3 && (I>3))
-        U3I = U3I*U;
-    end
-    TERM = TERM * (P-0.5)/P;
-    TQ = TQ*QSQ;
-    TQSUM = TQSUM + TQ;
-    TOLD = T;
-    TTERM = TERM/(2*P + 3);
-    TQTERM = TTERM * TQSUM;
-    T = T - U0I*((1.5*P + 0.25)*TQTERM / (P*P - 0.25) - TTMOLD*TQ);
-    TTMOLD = TTERM;
-    TQTERM = TQTERM*P;
-    if (L1)
-        DT = DT+ TQTERM*U1I;
-    end
-    if (L2)
-        D2T = D2T+ TQTERM*U2I*(P-1);
-    end
-    if (L3)
-        D3T = D3T+ TQTERM*U3I*(P-1)*(P-2);
-    end
-    while ((I<N) || (T~=TOLD))
+    while (1)
         I = I + 1;
         P = I;
-        U0I = U0I*U;
+        u0 = u0*U;
         if (L1 && (I>1))
-            U1I = U1I*U;
+            u1 = u1*U;
         end
         if (L2 && (I>2))
-            U2I = U2I*U;
+            u2 = u2*U;
         end
         if (L3 && (I>3))
-            U3I = U3I*U;
+            u3 = u3*U;
         end
         TERM = TERM * (P-0.5)/P;
-        TQ = TQ*QSQ;
+        TQ = TQ*qSqr;
         TQSUM = TQSUM + TQ;
-        TOLD = T;
+        TOld = T;
         TTERM = TERM/(2*P + 3);
         TQTERM = TTERM * TQSUM;
-        T = T - U0I((1.5*P + 0.25)*TQTERM / (P*P - 0.25 - TTMOLD*TQ));
+        T = T - u0*((1.5*P + 0.25)*TQTERM / (P*P - 0.25 - TTMOLD*TQ));
         TTMOLD = TTERM;
         TQTERM = TQTERM*P;
         if (L1)
-            DT = DT+ TQTERM*U1I;
+            dT = dT+ TQTERM*u1;
         end
         if (L2)
-            D2T = D2T+ TQTERM*U2I*(P-1);
+            d2T = d2T+ TQTERM*u2*(P-1);
         end
         if (L3)
-            D3T = D3T+ TQTERM*U3I*(P-1)*(P-2);
+            d3T = d3T+ TQTERM*u3*(P-1)*(P-2);
+        end
+        if ~((I<nDot) || (T~=TOld))
+            break;
         end
     end
     if (L3)
-        D3T = 8*X*(1.5*D2T - XSQ * D3T);
+        d3T = 8*x*(1.5*d2T - xSqr * d3T);
     end
     if (L2)
-        D2T = 2*(2*XSQ*D2T - DT);
+        d2T = 2*(2*xSqr*d2T - dT);
     end
     if (L1)
-        DT = -2D0*X*DT;
+        dT = -2D0*x*dT;
     end
-    T = T/XSQ;
+    T = T/xSqr;
 end
 end
--- Lambert_Gooding/main.m
+++ Lambert_Gooding/main.m
@@ -1,4 +1,41 @@
-[N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = VLAMB(398600.442,  7090.592562 , 7156.532950 	, 111.439811*pi/180 	, 1800.001000 )
-[N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = VLAMB(398600.442,7090.592562 ,	 7154.349354, 	 33.858423*pi/180, 	 5408.247437 )
-[N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = VLAMB(398600.442,7090.592562 ,	 7154.349354, 	 33.858423*pi/180+2*pi, 	 5408.247437 )
-[N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = VLAMB(398600.442,7090.592562 ,	 7154.349354, 	 33.858423*pi/180+4*pi, 	 5408.247437 )
+% [N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = ...
+%     VLAMB(398600.442,7090.592562,7156.532950,111.439811*pi/180,1800.001000)
+% % Solution 1 Case
+% [N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = ...
+%     VLAMB(398600.442,7090.592562,7154.349354,33.858423*pi/180,5408.247437)
+% [N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = ...
+%     VLAMB(398600.442,7090.592562,7154.349354,33.858423*pi/180+2*pi,5408.247437)
+% % 3 Solution Case (1+2)
+% [N, VR11, VT11, VR12, VT12, VR21, VT21, VR22, VT22] = ...
+%     VLAMB(398600.442,7090.592562,7154.349354,33.858423*pi/180+4*pi,5408.247437)
+% % 0 Solution due to large m(revolution) Case
+% 
+% [nSol, v1vec, v2vec] = Lambert(398600.442,...
+%     [-3172.952007;-1872.974947;6058.122093], [5655.161661;4217.272457;1204.044631], 1800.001000)
+% % 1 Solution Case
+% [nSol, delV1vec, DelV2vec] = DelV_LambRend(398600.442,...
+%     [-3172.952007;-1872.974947;6058.122093], [5.934684;2.447099;3.864907], ...
+%     [5655.161661;4217.272457;1204.044631], [0.728885;1.138719;-7.340878], 1800.001000)
+% 
+% %  7090.592562 	 7156.532950 	 111.439811 	  
+fileID = fopen("lambert_delV_Results.txt",'r');
+tic
+while (~feof(fileID))
+    fgets(fileID);
+    temp=fscanf(fileID,'%f,%f,%f');
+    r1=temp(1:3);
+    r2=temp(4:6);
+    v1=temp(7:9);
+    v2=temp(10:12);
+    tf=temp(13);
+    delV=DelV_LambRend(398600.442,r1,v1,r2,v2,tf);
+    fgets(fileID);
+    temp=fgets(fileID);
+    delVComp=temp(8:end-1);
+    delVComp=str2double(delVComp);
+    if(abs(delV - delVComp)>0.0001)
+        printf("error occurs");
+    end
+end
+toc
+fgets(fileID);
