af84ae0c0600bee30b5593e9cdd5d6db49422a0d
Jeff Ko <2929up@gmail.com>
2022-09-10 21:53:05 +0900

    Plot 일부 수정. Need Debugging

--- LowThrustQLawRV/main.m
+++ LowThrustQLawRV/main.m
@@ -10,8 +10,6 @@ ThrustMax = 1;                      % N
 Mmax      = 300;                    % kg
 Isp       = 3100;                   % sec
 g0        = 9.80665;                % m/s^2
-c         = Isp*g0;
-F         = ThrustMax / Mmax;
 J2        = 0;
 %--------------- Sun ------------------%
 % mu        = 1.32712440018*10^11;    % km^3/s^2
@@ -23,19 +21,20 @@ J2        = 0;
 % c         = Isp*g0;
 % F         = Tmax / Mmax;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Normalization
 AU2km   = Re;
 TU2sec  = sqrt(AU2km^3 / mu);
 TU2days = TU2sec / (60*60*24);
 MUkg    = Mmax;
-
 mu   = mu   / (AU2km^3/TU2sec^2);
 Re   = Re   / AU2km;
 ThrustMax = ThrustMax / (MUkg * AU2km * 1000 / TU2sec^2); %?
-Mmax = Mmax / MUkg; %?
+Mmax = Mmax / MUkg;
 Isp  = Isp  / TU2sec;
 g0   = g0   / (AU2km*1000 / TU2sec^2);
 c    = Isp * g0;
 F    = ThrustMax / Mmax;
+J2   = J2; %TODO : check need normalize
 
 %% Weight parameters
 Wa = 1;
@@ -48,7 +47,6 @@ W = [Wa; Wf; Wg; Wh; Wk] / norm([Wa; Wf; Wg; Wh; Wk]);
 
 %% chaser initial state 
 aChaser = 7000/AU2km;
-% aChaser = 7000;
 eChaser = 0.01;
 iChaser = 0.05*pi/180;
 oChaser = 0;
@@ -59,7 +57,6 @@ initialMass = Mmax;
 chaserState = [pChaser; fChaser; gChaser; hChaser; kChaser; LChaser; initialMass];
 
 aTarget = 42000/AU2km;
-% aTarget = 42000;
 eTarget = 0.01;
 iTarget = 0.05*pi/180;
 oTarget = 0;
@@ -84,7 +81,7 @@ QHist(1) = Q;
 
 %% Q-Guidance Simulation Start
 i = 1;
-tol = 100;
+tol = sqrt(sum(W));
 while 1
     
     F = ThrustMax / chaserState(7);
@@ -145,13 +142,13 @@ LTarget = linspace(LTarget,LTarget+2*pi,100);
 [rTargetOrbit,~] = Keplerian2GeocentricMIMO(pTarget,eTarget,iTarget,oTarget,wTarget,nuTarget);
 
 %% Low Thrust RV trajectory
-[a,e,i,o,w,nu] = Equinoctial2KeplerianMIMO(chaserTrajectory(1,:), chaserTrajectory(2,:), chaserTrajectory(3,:), chaserTrajectory(4,:), chaserTrajectory(5,:) ,chaserTrajectory(6,:));
+[a,e,i,o,w,nuArr] = Equinoctial2KeplerianMIMO(chaserTrajectory(1,:), chaserTrajectory(2,:), chaserTrajectory(3,:), chaserTrajectory(4,:), chaserTrajectory(5,:) ,chaserTrajectory(6,:));
 
-[r,~] = Keplerian2GeocentricMIMO(a,e,i,o,w,nu);
+[r,~] = Keplerian2GeocentricMIMO(a,e,i,o,w,nuArr);
 
 %% plot
 figure
-subplot(2,3,1)
+subplot(3,3,1)
 hold on
 plot3(rChaserOrbit(1,:),rChaserOrbit(2,:),rChaserOrbit(3,:),'b','LineWidth',3)
 plot3(rTargetOrbit(1,:),rTargetOrbit(2,:),rTargetOrbit(3,:),'r','LineWidth',3)
@@ -164,55 +161,76 @@ ylabel('y')
 zlabel('z')
 legend('initial orbit','final orbit','transfer orbit')
 
-pp1 = chaserTrajectory(1,:);
-ff1 = chaserTrajectory(2,:);
-gg1 = chaserTrajectory(3,:);
-hh1 = chaserTrajectory(4,:);
-kk1 = chaserTrajectory(5,:);
-LL1 = chaserTrajectory(6,:);
+pArr = chaserTrajectory(1,:);
+fArr = chaserTrajectory(2,:);
+gArr = chaserTrajectory(3,:);
+hArr = chaserTrajectory(4,:);
+kArr = chaserTrajectory(5,:);
+LArr = chaserTrajectory(6,:);
 
-[aa1,ecc1,inc1,Ome1,ome1,nu1] = lowThrustMee2Coe(pp1, ff1, gg1, hh1, kk1, LL1);
+[aArr,eArr,iArr,oArr,wArr,nuArr] = Equinoctial2KeplerianMIMO(pArr, fArr, gArr, hArr, kArr, LArr);
 
-pp2 = targetTrajectory(1,:);
-ff2 = targetTrajectory(2,:);
-gg2 = targetTrajectory(3,:);
-hh2 = targetTrajectory(4,:);
-kk2 = targetTrajectory(5,:);
-LL2 = targetTrajectory(6,:);
+pTarget = targetTrajectory(1,:);
+fTarget = targetTrajectory(2,:);
+gTarget = targetTrajectory(3,:);
+hTarget = targetTrajectory(4,:);
+kTarget = targetTrajectory(5,:);
+LTarget = targetTrajectory(6,:);
 
-[aa2,ecc2,inc2,Ome2,ome2,nu2] = lowThrustMee2Coe(pp2, ff2, gg2, hh2, kk2, LL2);
+[aTarget,eTarget,iTarget,oTarget,wTarget,~] = Equinoctial2KeplerianMIMO(pTarget, fTarget, gTarget, hTarget, kTarget, LTarget);
 
-subplot(2,3,2)
-plot(time*TU2days,aa1*AU2km)
+subplot(3,3,2)
+plot(time*TU2days,aArr*AU2km)
 hold on
-plot(time*TU2days,aa2*AU2km)
+plot(time*TU2days,aTarget*AU2km)
 xlabel('time (days)')
 ylabel('semi-major axis (km)')
 
-subplot(2,3,3)
-plot(time*TU2days,ecc1)
+subplot(3,3,3)
+plot(time*TU2days,eArr)
 hold on
-plot(time*TU2days,ecc2)
+plot(time*TU2days,eTarget)
 xlabel('time (days)')
 ylabel('eccentricity (-)')
 
-subplot(2,3,4)
-plot(time*TU2days,inc1*180/pi)
+subplot(3,3,4)
+plot(time*TU2days,iArr*180/pi)
 hold on
-plot(time*TU2days,inc2*180/pi)
+plot(time*TU2days,iTarget*180/pi)
 xlabel('time (days)')
 ylabel('inclination (deg)')
 
-subplot(2,3,5)
-plot(time*TU2days,Ome1*180/pi)
+subplot(3,3,5)
+plot(time*TU2days,oArr*180/pi)
 hold on
-plot(time*TU2days,Ome2*180/pi)
+plot(time*TU2days,oTarget*180/pi)
 xlabel('time (days)')
 ylabel('RAAN (deg)')
 
-subplot(2,3,6)
-plot(time*TU2days,ome1*180/pi)
+subplot(3,3,6)
+plot(time*TU2days,wArr*180/pi)
 hold on
-plot(time*TU2days,ome2*180/pi)
+plot(time*TU2days,wTarget*180/pi)
 xlabel('time (days)')
 ylabel('argument of perigee (deg)')
+
+subplot(3,3,7)
+plot(time*TU2days,fArr)
+hold on
+plot(time*TU2days,fTarget)
+xlabel('time (days)')
+ylabel('f')
+
+subplot(3,3,8)
+plot(time*TU2days,gArr)
+hold on
+plot(time*TU2days,gTarget)
+xlabel('time (days)')
+ylabel('g')
+
+subplot(3,3,9)
+plot(time*TU2days,hArr)
+hold on
+plot(time*TU2days,hTarget)
+xlabel('time (days)')
+ylabel('k')
